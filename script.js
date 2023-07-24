5de0a260e9273c069f7f078250f8ebbf276127f5
Sathishramesh1 <130343111+Sathishramesh1@users.noreply.github.com>
2023-07-24 13:43:20 +0530

    callback hell timer

---
+++ script.js
@@ -0,0 +1,41 @@
+
+setTimeout(() => {
+    document.getElementById("no").innerHTML="Countdown Starts"
+setTimeout(() => {
+    document.getElementById("no").innerHTML="10";
+    setTimeout(() => {
+        document.getElementById("no").innerHTML="9";
+        setTimeout(() => {
+            document.getElementById("no").innerHTML="8";
+            setTimeout(() => {
+                document.getElementById("no").innerHTML="7";
+                setTimeout(() => {
+                    document.getElementById("no").innerHTML="6";
+                    setTimeout(() => {
+                        document.getElementById("no").innerHTML="5";
+                        setTimeout(() => {
+                            document.getElementById("no").innerHTML="4";
+                            setTimeout(() => {
+                                document.getElementById("no").innerHTML="3";
+                                setTimeout(() => {
+                                    document.getElementById("no").innerHTML="2";
+                                    setTimeout(() => {
+                                        document.getElementById("no").innerHTML="1";
+                                        setTimeout(() => {
+                                            document.getElementById("no").innerHTML="Happy Independence Day!!!!!";
+                                        }, 1000);
+                                    }, 1000);
+
+                                }, 1000);
+                                
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+        
+    }, 1000);
+    
+}, 1000);
+}, 500);
