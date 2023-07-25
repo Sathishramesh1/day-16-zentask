4fb62161f6519f70e1e37c86d0de051cf8900f8a
Sathishramesh1 <130343111+Sathishramesh1@users.noreply.github.com>
2023-07-25 08:04:26 +0530

    removing extra lines of code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//apporach-1
+
 setTimeout(() => {
     document.getElementById("no").innerHTML="Countdown Starts"
 setTimeout(() => {
@@ -39,26 +39,3 @@ setTimeout(() => {
     
 }, 1000);
 }, 500);
-
-
-//apporoch-2 The above same also done using below code
-
-// const time=(delay,no)=>{
-//     const h1=document.getElementById("no")
-//     setTimeout(() => {
-//         h1.innerHTML=no;
-//     }, delay);
-    
-//     }
-//     time(500,"countdown starts")
-//     time(1000,10);
-//     time(2000,9);
-//     time(3000,8);
-//     time(4000,7);
-//     time(5000,6);
-//     time(6000,5);
-//     time(7000,4);
-//     time(8000,3);
-//     time(9000,2);
-//     time(10000,1);
-//     time(11000,"happy indepence day");
